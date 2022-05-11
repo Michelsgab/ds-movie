f29adc7824e6df3a27f2997b553a39462563b8f1
cbsilva <cbsilva@magnasistemas.com.br>
2022-05-10 21:06:41 -0300

    feat: "MovieCard - Final - Class1"

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,41 @@
-import MovieScore from "components/MovieScore";
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 export default function Listing() {
   return (
     <>
       <Pagination />
-      <MovieScore />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-4 col-md-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-4 col-md-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-4 col-md-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-4 col-md-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-4 col-md-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-4 col-md-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-4 col-md-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-4 col-md-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-4 col-md-4 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
